8ee2a33523418af5f040119ff7ce04cf86a67fbe
OWL81 <irhir.mohamed@gmail.com>
2023-05-18 15:50:42 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- *  *  main -Entry point
- *   *
- *    *  Return: Always 0 (Success)
- *     */
+ * main - This function prints "Programming is like building a multilingual puzzle" and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
+
