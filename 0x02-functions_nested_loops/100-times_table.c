b3f4c9239d8dcce90f4f04c7621a81bdebdfd377
OWL81 <irhir.mohamed@gmail.com>
2023-05-23 16:06:02 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int row, col, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				product = row * col;
+				if (col == 0)
+				{
+					_putchar(product + '0');
+				}
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else if (product >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
