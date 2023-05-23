a61d42dddc16a3991f680ba0d5f157d35254a7d0
OWL81 <irhir.mohamed@gmail.com>
2023-05-23 16:20:11 -0700

    task 11 C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,41 @@
 #include "main.h"
 
 /**
+ * print_spaces - prints spaces.
+ * @n: the number of spaces to print.
+ */
+void print_spaces(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+}
+
+/**
+ * print_number - prints an integer as characters.
+ * @n: the number to print.
+ */
+void print_number(int n)
+{
+	if (n >= 100)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
+
+/**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
@@ -15,34 +50,18 @@ void print_times_table(int n)
 			for (col = 0; col <= n; col++)
 			{
 				product = row * col;
-				if (col == 0)
-				{
-					_putchar(product + '0');
-				}
-				else if (product < 10)
+
+				if (col != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
+					if (product < 100)
+						print_spaces(1);
+					if (product < 10)
+						print_spaces(1);
 				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else if (product >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((product / 100) + '0');
-					_putchar(((product / 10) % 10) + '0');
-					_putchar((product % 10) + '0');
-				}
+
+				print_number(product);
 			}
 			_putchar('\n');
 		}
