270189b192c2b59211358e84895fc7234c8f622f
OWL81 <irhir.mohamed@gmail.com>
2023-05-25 05:56:59 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if integer is positive or negative
+ * Return: 0 if the program completed successfully, or a non-zero number
+ */
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+	positive_or_negative(number);
+
+	return (0);
+}
+
