a7dcad75021127b894a2e6d95c95955d5f09687e
OWL81 <irhir.mohamed@gmail.com>
2023-05-29 16:39:37 -0700

    first task 0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates the value to 98.
+ * @n: Pointer to an int.
+ *
+ * Description:
+ * This function takes a pointer to an int as a parameter and updates the
+ * value it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
